5dfafbbb836597134b8f8179c599b53f0c9d71cc
Anamaria Miranda <anny13miranda@gmail.com>
2024-05-24 15:03:22 -0500

    Disabled verification

--- src/modules/prisma/prisma.service.ts
+++ src/modules/prisma/prisma.service.ts
@@ -5,11 +5,11 @@ import { PrismaClient } from '@prisma/client';
 export class PrismaService extends PrismaClient implements OnModuleInit {
   private logger = new Logger('Services up');
   async onModuleInit() {
-    try {
-      await this.user.findMany({ take: 1 });
-      this.logger.log('Successfully connected to the database.');
-    } catch (error) {
-      this.logger.log('Failed to connect to the database.', error);
-    }
+    // try {
+    //   await this.user.findMany({ take: 1 });
+    //   this.logger.log('Successfully connected to the database.');
+    // } catch (error) {
+    //   this.logger.log('Failed to connect to the database.', error);
+    // }
   }
 }
